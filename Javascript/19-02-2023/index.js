3c654d579710dbdb00915ae89fe1ba73e9b3a807
Protik Sarkar <protiksarkar15@gmail.com>
2023-02-26 09:09:14 +0530

    created Javascript structure

---
+++ Javascript/19-02-2023/index.js
@@ -0,0 +1,50 @@
+console.log('This is Protik Sarkar');
+var a =10;
+console.log(a++);
+
+function sum(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+console.log(sum(1,2));
+console.log(subtract(3,2));
+console.log(multiply(2,4));
+console.log(divide(9,3));
+
+console.log(B);
+var B = 10;
+console.log(B);
+
+
+var aa = 10;
+console.log(aa,bb,cc);
+var cc = 20;
+console.log(aa,bb,cc);
+var bb;
+bb=30;
+console.log(aa,bb,cc);
+
+aaa=1;
+console.log(aaa);
+
+
+function print2() {
+    aaaa=10;
+    console.log('1>',aaaa);
+    var aaaa;
+}
+
+print2();
+console.log('2>',aaaa);
