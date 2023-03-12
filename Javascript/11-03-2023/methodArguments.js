c8c1e5e21f56fb392a098071e9cab16ef00197e0
Protik Sarkar <protiksarkar15@gmail.com>
2023-03-12 09:14:54 +0530

    11th Mar commits

--- Javascript/11-03-2023/methodArguments.js
+++ Javascript/11-03-2023/methodArguments.js
@@ -16,39 +16,6 @@ console.log(sum1(1,2,3));
 console.log(sum1(1,2));
 
 console.log("------------------");
-function sum2(...args) {
-
-    console.log(args);
-
-    return args.reduce(function(acc, item) {
-        acc += item;
-        return acc;
-    }, 0);
-   
-}
-
-console.log(sum2(1,2,3,4,5));
-
-console.log(sum2(1,2,3));
-
-console.log(sum2(1,2));
-
-
-console.log("------------------");
-
-
-function combineArray(...args) {
-  
-    return args.reduce(function(acc, item) {
-        item.forEach(element => {
-            acc.push(element);
-        });
-        // acc =acc.concat(item);
-        return acc;
-    }, []);
-}
-
-console.log(combineArray([1,2,3],[4,5,6,7],[8,9])); // => [1,2,3,4,5,6,7,8,9]
 
 console.log("------------------");
 
